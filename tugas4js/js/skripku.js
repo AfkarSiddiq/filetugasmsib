4aa133b9978f070cdfd42a50022f0f69c0d9b5f1
afkar <afkarsiddiq0@gmail.com>
2023-03-17 10:26:15 +0700

    revisi

--- tugas4js/js/skripku.js
+++ tugas4js/js/skripku.js
@@ -1,5 +1,5 @@
 function login(){
-    var username = document.getElementById("email").value;
+    var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
     
     if (username == "mimin" && password == "mimin123"){
